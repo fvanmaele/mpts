62b5acc720fbf1e26ef864d288e33ce3c6fa2eec
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2023-06-25 20:32:26 +0200

    WIP: linear_forest()

--- python/max_linear_forest.py
+++ python/max_linear_forest.py
@@ -17,27 +17,29 @@ from scipy import sparse
 
 # %%
 def sparse_is_symmetric(mtx, tol=1e-10):
+    """Check a matrix for numerical symmetry
+    """
     assert sparse.issparse(mtx)
 
     return (abs(mtx-mtx.T) > tol).nnz == 0
 
 
-"""Sequential greedy [0,n]-factor computation on a weighted graph G = (V, E)
-"""
 def find_n_factors(mtx, n):
+    """Sequential greedy [0,n]-factor computation on a weighted graph G = (V, E)
+    """
     assert sparse_is_symmetric(mtx)
     assert n > 0
 
     # factors = [[] for _ in range(0, mtx.shape[0])]
-    G  = nx.Graph(abs(mtx))                    # absolute values for edge weights
-    G.remove_edges_from(nx.selfloop_edges(G))  # ignore self-loops (diagonal elements)
+    G_abs = nx.Graph(abs(mtx))  # absolute values for edge weights
+    G_abs.remove_edges_from(nx.selfloop_edges(G_abs))  # ignore self-loops (diagonal elements)
 
     Gn = nx.Graph()
-    Gn.add_nodes_from(G)
+    Gn.add_nodes_from(G_abs)
     
     # Iterate over edges in decreasing weight order
-    for v, w, O in sorted(G.edges(data=True), key=lambda t: t[2].get('weight', 1), reverse=True):
-        assert(v != w)
+    for v, w, O in sorted(G_abs.edges(data=True), key=lambda t: t[2].get('weight', 1), reverse=True):
+        assert(v != w)  # sanity check
 
         # Empty set, one vertex, ..., or n vertices from the neighborhood of a vertex v
         if Gn.degree[v] < n and Gn.degree[w] < n:
@@ -55,28 +57,19 @@ def linear_forest(mtx):
     forest = nx.Graph()
     forest.add_nodes_from(Gn)
 
-    # # Break cycles by removing edge of smallest (absolute) weight
-    # for v, nbh in enumerate(factors):
-    #     if len(nbh) < 2:
-    #         forest[v] = copy(nbh)
+    for c in nx.connected_components(Gn):
+        Gc = Gn.subgraph(c).copy()
+        try:
+            nx.find_cycle(Gn.subgraph(c))
+            # TODO: Break cycle by removing edge of smallest weight
+            
+        except nx.NetworkXNoCycle:
+            # OK, add to forest
+            forest.update(Gc)
 
-    #     elif nbh[0] in factors[nbh[1]]:
-    #         # cycle found, remove smallest of three edges
-    #         w = [G_abs.get_edge_data(v, nbh[0])['weight'],
-    #              G_abs.get_edge_data(v, nbh[1])['weight'],
-    #              G_abs.get_edge_data(nbh[0], nbh[1])['weight']
-    #         ]
-    #         amin = np.argmin(w)
-    #         if amin == 0:
-    #             forest[v] = [nbh[1]]
-    #         elif amin == 1:
-    #             forest[v] = [nbh[0]]
-    #         elif amin == 2:
-    #             pass
+    return forest
 
-    #     else:
-    #         # no cycle found, add path
-    #         forest[v] = copy(nbh)
 
-    # # XXX: return graph
-    # return forest
+# TODO: permute (adjacency matrix of) linear forest to tridiagonal form
+def linear_forest_permute(mtx):
+    pass
