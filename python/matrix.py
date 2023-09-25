fix diagp1 normalization

python/matrix.py
@@ -11,33 +11,48 @@ from pathlib       import Path
 from scipy.io      import mmread
 from sparse_util   import s_coverage, s_degree, sparse_ddiag
 from precond       import spanning_tree_precond, linear_forest_precond
-from precond_diag  import diagp0
+from precond_diag  import diagp0, diagp1
 
+import numpy as np
 
 # %%
-def main(mtx_l, normalize_diag):
+def main(mtx_l):
     print('name,diag,max_lf,max_st,diag_dom,max_deg')
 
     for mtx_str in mtx_l:
         mtx  = sparse.coo_matrix(mmread(mtx_str))
         n, m = mtx.shape
+        
+        # Skip rectangular matrices
         if n != m:
             continue
 
+        # Skip matrices with zero rows (<=> diagp1 is not invertible)
+        mtx_diagp1 = diagp1(mtx)
+        if not np.all(mtx_diagp1):
+            continue
+
+        # For purposes of comparing the S-coverage (between linear forest and
+        # maximum spanning tree), we first normalize the matrix by multiplying
+        # with the inverse of diagp_1.
+        mtx = (sparse.diags(1 / mtx_diagp1) @ mtx).tocoo()
+
+        # Compute matrix properties
         mtx_name   = Path(mtx_str).stem
-        mtx_mdeg   = s_degree(mtx)
-        mtx_dd     = sparse_ddiag(mtx)
-        mtx_diag   = sparse.diags(diagp0(mtx))
-        
+        mtx_mdeg   = s_degree(mtx)              # maximum degree of corresponding graph (without loops)
+        mtx_dd     = sparse_ddiag(mtx)          # diagonally dominant rows
+        mtx_diag   = sparse.diags(diagp0(mtx))  # diagonal component
+
+        # Compute graph preconditioner
         try:
             mtx_max_lf = linear_forest_precond(mtx)
             mtx_max_st = spanning_tree_precond(mtx)
         except ValueError:
             continue
 
-        cvg_diag   = s_coverage(mtx, mtx_diag,   normalize_diag=normalize_diag)
-        cvg_max_lf = s_coverage(mtx, mtx_max_lf, normalize_diag=normalize_diag)
-        cvg_max_st = s_coverage(mtx, mtx_max_st, normalize_diag=normalize_diag)
+        cvg_diag   = s_coverage(mtx, mtx_diag)
+        cvg_max_lf = s_coverage(mtx, mtx_max_lf)
+        cvg_max_st = s_coverage(mtx, mtx_max_st)
 
         print(f'{mtx_name},{cvg_diag},{cvg_max_lf},{cvg_max_st},{mtx_dd},{mtx_mdeg}')
 
@@ -45,9 +60,7 @@ def main(mtx_l, normalize_diag):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog='matrix', description='analyze properties of a matrix')
-    parser.add_argument('--normalize-diag', action='store_true',
-                        help='multiply with diagp1 from the left (s-coverage)')
     parser.add_argument('mtx', nargs='+')
     
     args = parser.parse_args()
-    main(args.mtx, args.normalize_diag)
+    main(args.mtx)

python/precond_diag.py
@@ -3,13 +3,14 @@
 """
 Created on Mon Sep 18 19:44:15 2023
 
+Diagonal preconditioners
+
 @author: archie
 """
 
 import numpy as np
 
 
-# %% Diagonal preconditioners
 def row_sums_excluding_diagonal(mtx):
     # Set the diagonal elements to zero in the CSR matrix
     mtx = mtx.copy()
@@ -18,18 +19,23 @@ def row_sums_excluding_diagonal(mtx):
     # Compute the sum of row elements excluding the diagonal elements
     return np.array(mtx.sum(axis=1)).flatten()
 
+
 def unit(x):
     x[x == 0] = 1
     return x / np.abs(x)
 
+
 def diagp0(mtx):
     return mtx.diagonal()
+
 
 def diagp1(mtx):
     return np.multiply(unit(diagp0(mtx)), np.maximum(np.abs(diagp0(mtx)), row_sums_excluding_diagonal(abs(mtx))))
 
+
 def diagp2(mtx):
     return np.multiply(unit(diagp0(mtx)), np.array(abs(mtx).sum(axis=1)).flatten())
+
 
 def diagl1(mtx):
     return diagp0(mtx) + row_sums_excluding_diagonal(abs(mtx))

python/sparse_util.py
@@ -9,6 +9,7 @@ import warnings
 import numpy as np
 from scipy import sparse
 
+
 # %%
 def sparse_is_symmetric(mtx, tol=1e-10):
     """ Check a matrix for numerical symmetry
@@ -17,7 +18,7 @@ def sparse_is_symmetric(mtx, tol=1e-10):
     return (abs(mtx-mtx.T) > tol).nnz == 0
 
 
-def sparse_ddiag(matrix):
+def sparse_ddiag(matrix, strict=False):
     if not sparse.issparse(matrix):
         raise ValueError("Input matrix must be a Scipy sparse matrix.")
 
@@ -26,7 +27,10 @@ def sparse_ddiag(matrix):
     abs_diagonal = abs(matrix.diagonal())
 
     # A1: Return self as flattened ndarray
-    return np.count_nonzero((abs_diagonal >= abs_row_sums_without_diagonal).A1) / matrix.shape[0]
+    if strict:
+        return np.count_nonzero((abs_diagonal > abs_row_sums_without_diagonal).A1) / matrix.shape[0]
+    else:
+        return np.count_nonzero((abs_diagonal >= abs_row_sums_without_diagonal).A1) / matrix.shape[0]
 
 
 def sparse_prune(mtx, mtx_mask):
@@ -86,15 +90,11 @@ def sparse_scale(mtx, mtx_mask, scale):
     return sparse.coo_matrix((data, (rows, cols)))
 
 
-def s_coverage(mtx, mtx_pruned, normalize_diag=False):
+def s_coverage(mtx, mtx_pruned):
     """ Compute the S-coverage as quality measure for a sparse preconditioner
     """
     assert sparse.issparse(mtx)
     assert sparse.issparse(mtx_pruned)
-
-    if normalize_diag:
-        mtx.setdiag(1)
-        mtx_pruned.setdiag(1)
 
     sc = abs(sparse.csr_matrix(mtx_pruned)).sum() / abs(sparse.csr_matrix(mtx)).sum()
     if sc > 1: